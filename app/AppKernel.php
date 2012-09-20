b9792803b88aceb62b26617b25d08440b9612c0c
David Stangeby <david@arcticsolutions.no>
2012-09-20 19:13:20 +0200

    Revert "Added a case bundle"
    This reverts commit 8fc9d787dfd37772f993baa624ffdc102e83e955.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,7 +22,6 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Arctic\AssetBundle\ArcticAssetBundle(),
             new Arctic\UserBundle\ArcticUserBundle(),
-            new Arctic\CaseBundle\ArcticCaseBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/Arctic/CaseBundle/ArcticCaseBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Arctic\CaseBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ArcticCaseBundle extends Bundle
-{
-}
