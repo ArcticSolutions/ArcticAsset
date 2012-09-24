b8895270f147bb0b119128b9184fad005ca4614e
David Stangeby <david@arcticsolutions.no>
2012-09-24 15:04:45 +0200

    Added a Category entity.
    Category entity for asset types.

---
+++ src/Arctic/AssetBundle/Entity/Category.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace Arctic\AssetBundle\Entity;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Arctic\AssetBundle\Entity\Category
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Arctic\AssetBundle\Entity\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime $created
+     *
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+    /**
+     * @var \DateTime $updated
+     *
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(name="updated", type="datetime")
+     */
+    private $updated;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Type", mappedBy="category")
+     */
+    protected $types;
+
+    public function __construct()
+    {
+        $this->types = new ArrayCollection();
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return Category
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set updated
+     *
+     * @param \DateTime $updated
+     * @return Category
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+    
+        return $this;
+    }
+
+    /**
+     * Get updated
+     *
+     * @return \DateTime 
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Add types
+     *
+     * @param Arctic\AssetBundle\Entity\Type $types
+     * @return Category
+     */
+    public function addType(\Arctic\AssetBundle\Entity\Type $types)
+    {
+        $this->types[] = $types;
+    
+        return $this;
+    }
+
+    /**
+     * Remove types
+     *
+     * @param Arctic\AssetBundle\Entity\Type $types
+     */
+    public function removeType(\Arctic\AssetBundle\Entity\Type $types)
+    {
+        $this->types->removeElement($types);
+    }
+
+    /**
+     * Get types
+     *
+     * @return Doctrine\Common\Collections\Collection 
+     */
+    public function getTypes()
+    {
+        return $this->types;
+    }
+}
--- src/Arctic/AssetBundle/Entity/Type.php
+++ src/Arctic/AssetBundle/Entity/Type.php
@@ -54,6 +54,12 @@ class Type
     private $model;
 
     /**
+     * @ORM\ManyToOne(targetEntity="Category", inversedBy="types")
+     * @ORM\JoinColumn(name="category_id", referencedColumnName="id")
+     */
+    private $category;
+
+    /**
      * @ORM\OneToMany(targetEntity="Asset", mappedBy="type")
      */
     protected $assets;
@@ -192,5 +198,28 @@ class Type
     public function getUpdated()
     {
         return $this->updated;
+    }
+
+    /**
+     * Set category
+     *
+     * @param Arctic\AssetBundle\Entity\Category $category
+     * @return Type
+     */
+    public function setCategory(\Arctic\AssetBundle\Entity\Category $category = null)
+    {
+        $this->category = $category;
+    
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return Arctic\AssetBundle\Entity\Category 
+     */
+    public function getCategory()
+    {
+        return $this->category;
     }
 }
