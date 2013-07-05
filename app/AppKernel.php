a8dcd2b8f751e10df9cd52f44646b0d75176112b
David Stangeby <david@arcticsolutions.no>
2013-07-06 00:43:33 +0200

    Updated twitter bootstrap and jquery

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,7 +24,7 @@ class AppKernel extends Kernel
             new Arctic\UserBundle\ArcticUserBundle(),
             new Arctic\TicketBundle\ArcticTicketBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-            new Arctic\DashboardBundle\ArcticDashboardBundle(),
+            new Arctic\DashboardBundle\ArcticDashboardBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
